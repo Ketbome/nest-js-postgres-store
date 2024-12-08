534d022ef2518ff73b3927e994a3f5eef47fa88c
Ketbome <Pablo.moraga.san@gmail.com>
2024-12-08 13:53:19 -0300

    feat: :sparkles: Add ApiProperty decorators to DTOs for improved Swagger documentation

--- src/users/dtos/customer.dto.ts
+++ src/users/dtos/customer.dto.ts
@@ -1,17 +1,20 @@
 import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
 export class CreateCustomerDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: `customer's name` })
   readonly name: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: `customer's last name` })
   readonly lastName: string;
 
   @IsPhoneNumber()
   @IsNotEmpty()
+  @ApiProperty({ description: `customer's phone number` })
   readonly phone: string;
 }
 
--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,4 +1,10 @@
-import { IsString, IsNotEmpty, IsEmail, Length } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  Length,
+  IsPositive,
+} from 'class-validator';
 import { PartialType, ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
@@ -10,10 +16,16 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   @Length(6)
+  @ApiProperty({ description: 'the password of user' })
   readonly password: string;
 
   @IsNotEmpty()
+  @ApiProperty({ description: 'the role of user' })
   readonly role: string;
+
+  @IsPositive()
+  @ApiProperty({ description: 'the id of customer' })
+  readonly customerId: number;
 }
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
